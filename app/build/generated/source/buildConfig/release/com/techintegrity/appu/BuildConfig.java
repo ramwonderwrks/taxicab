75d6f9dcc81f11e70ec3d2f87edf5b316a7461de
ramwonderwrks <ramk@wonderwrks.com>
2017-07-13 17:01:52 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/release/com/techintegrity/appu/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.techintegrity.appu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.techintegrity.appu";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
